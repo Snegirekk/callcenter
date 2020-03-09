7248316f782d28c328abf858f6107f134a315f67
Snegirekk <zlobin.n.s@gmail.com>
2020-03-09 12:06:50 +0700

    [dto] create dtos

---
+++ src/main/java/com/snegirekk/callcenter/dto/ListCallTaskDto.java
@@ -0,0 +1,10 @@
+package com.snegirekk.callcenter.dto;
+
+import java.util.Date;
+
+public class ListCallTaskDto {
+
+    public String orderNumber;
+    public Date addedAt;
+    public boolean isDone;
+}
---
+++ src/main/java/com/snegirekk/callcenter/dto/NewCallTaskDto.java
@@ -0,0 +1,8 @@
+package com.snegirekk.callcenter.dto;
+
+import java.util.UUID;
+
+public class NewCallTaskDto {
+
+    public UUID orderId;
+}
