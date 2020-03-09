89645e733026bc518a99d76e4e4e8cf248530d7c
Snegirekk <zlobin.n.s@gmail.com>
2020-03-09 12:09:41 +0700

    [config] configure jackson

---
+++ src/main/java/com/snegirekk/callcenter/config/AppConfig.java
@@ -0,0 +1,34 @@
+package com.snegirekk.callcenter.config;
+
+import com.snegirekk.callcenter.dto.ListCallTaskDto;
+import com.snegirekk.callcenter.entity.CallTask;
+import org.modelmapper.Converter;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        Converter<CallTask, ListCallTaskDto> callTaskConverter = mappingContext -> {
+            CallTask entity = mappingContext.getSource();
+            ListCallTaskDto dto = mappingContext.getDestination();
+
+            if (null == dto) {
+                dto = new ListCallTaskDto();
+            }
+
+            dto.orderNumber = entity.getOrder().getOrderNumber();
+            dto.addedAt = entity.getCreatedAt();
+
+            return dto;
+        };
+
+        ModelMapper mapper = new ModelMapper();
+        mapper.addConverter(callTaskConverter);
+
+        return mapper;
+    }
+}
